9c386dbbbd026a7facfca5f0181196a151183c9f
KidIcarus1337 <kid_icarus3@yahoo.com>
2016-02-29 19:30:03 -0600

    Further development on AddItemBar.

--- scripts/components/AddItemBar.js
+++ scripts/components/AddItemBar.js
@@ -5,9 +5,16 @@ import autobind from 'autobind-decorator';
 class AddItemBar extends React.Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <li className="add-item-bar">
+        <div>
+          <div className="add-item-count">
+            <input type="text" ref="add-item-count" />
+          </div>
+          <div className="add-item-name">
+            <input type="text" ref="add-item-name" />
+          </div>
+        </div>
+      </li>
     )
   }
 }
